23d746c5f965abe7d4a1cb7d79387c9dc209d084
s l e e p y <143356794+2facvd@users.noreply.github.com>
2023-12-05 11:39:53 +0100

    Add files via upload

---
+++ ProGraMaUtilities/ProfileBackgrounds by Devork/scripts/CustomProfileBackgrounds.js
@@ -0,0 +1,31 @@
+const InsertBeforeLoad = async () => {
+	const DESCRIPTION_TEXT = document.querySelector('#description-extend > div > div.text').innerHTML;
+	const BACKGROUND_AVATAR = document.querySelector('.background-avatar')
+	const BACKGROUND_SECTION = document.querySelector('.section-top-background')
+	const BACKGROUND_REGEXP = /background:\s*(\d+)(?:,\s*filter:\s*(light|dark|blur|none))?;/i
+	const BACKGROUND_DETAILS = BACKGROUND_REGEXP.exec(DESCRIPTION_TEXT)
+
+	if (typeof BACKGROUND_DETAILS == 'object') {
+		const FETCH = (await (await fetch(`/game/published/?q=${BACKGROUND_DETAILS[1]}`)).json())
+		const IMAGE = FETCH.images.large.replace('cache', 'images').replace('_600x240.jpg', '.png')
+
+		BACKGROUND_AVATAR.style.backgroundImage = `url(${IMAGE})`;
+
+		switch (BACKGROUND_DETAILS[2]) {
+			case 'blur':
+				BACKGROUND_AVATAR.style.opacity = 'unset';
+				BACKGROUND_SECTION.style.backgroundImage = 'none';
+				break;
+			case 'none':
+				BACKGROUND_AVATAR.style.opacity = 'unset';
+				BACKGROUND_AVATAR.style.filter = 'none';
+				BACKGROUND_SECTION.style.backgroundImage = 'none';
+				break;
+			case 'dark':
+				BACKGROUND_SECTION.style.backgroundImage = 'none';
+				break;
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', InsertBeforeLoad);
