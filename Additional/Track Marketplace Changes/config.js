b4b3dab57b2d9977d8ae08a199faefea6a479bb9
aztro <143356794+zombieaztro@users.noreply.github.com>
2023-12-20 17:47:10 +0100

    Delete Additional directory

--- Additional/Track Marketplace Changes/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  TargetID: ' ',
-  models: true,
-  avatars: true,
-  scanInterval: 5, // in seconds
-};
--- Additional/Track Marketplace Changes/vivuar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing dependencies...
-npm install axios cheerio
-echo Dependencies installed.
-
-echo Running vivuar.js...
-node vivuar.js
