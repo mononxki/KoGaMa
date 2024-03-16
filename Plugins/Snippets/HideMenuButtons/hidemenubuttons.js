b6eefd5569b6bc7449fa9f44bd26864e8c0097d6
⛧ sim <143356794+suchsad@users.noreply.github.com>
2024-03-16 22:16:38 +0100

    Delete Plugins directory

--- Plugins/Snippets/HideMenuButtons/hidemenubuttons.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('a[href="/videos/"]').style.display = 'none';
-    document.querySelector('a[href="/subscription/subscribe/"]').style.display = 'none';
-      document.querySelector('a[href="/purchase/"]').style.display = 'none';
-    document.querySelector('a[href="/news/"]').style.display = 'none';
-
